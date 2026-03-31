ec3dded5dfabc931bbfe221e23d21aaa0bb3be54
Patrick Brennan <brennanpatrick20@gmail.com>
2026-03-31 10:47:19 -0400

    Switch to arduino implementation of map_range

--- main/util.hpp
+++ main/util.hpp
@@ -13,10 +13,5 @@ template <typename T,typename M> T deadzone(T val, M deadzone) {
 }
 
 template <typename T> T map_range(T value, T a_start, T a_end, T b_start, T b_end) {
-  // Source - https://stackoverflow.com/a/5732390
-  // Posted by Alok Singhal
-  // Retrieved 2026-02-27, License - CC BY-SA 3.0
-
-  double slope = 1.0 * (b_end - b_start) / (a_end - a_start);
-  return (T) (b_start + slope * (value - a_start));
+  return (value - a_start) * (b_end - b_start) / (a_end - a_start) + b_start;
 }
